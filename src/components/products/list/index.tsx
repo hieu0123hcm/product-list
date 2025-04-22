7fd3321c297020df8c562fbc9fc4d78fa94ac191
Nguyễn Thanh Hiếu <43198687+hieu0123hcm@users.noreply.github.com>
2025-04-22 15:48:48 +0700

    refactor: make code cleaner, and follow principles

--- src/components/products/list/index.tsx
+++ src/components/products/list/index.tsx
@@ -1,39 +1,64 @@
 import { useEffect, useState } from 'react';
-import { getSkipValue } from '@utils/paging';
-import useFetch from '@hooks/useFetch';
-import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import { debounce } from 'lodash';
+
+import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import { ProductCard } from '@components/products';
+import Input from '@components/common/input-field';
+import useProductFetch from '@hooks/useProductFetch';
+
 import './list.css';
 
 const PRODUCTS_PER_PAGE = 20;
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500; //miliseconds
+const DEFAULT_PAGE = 1;
+const BOTTOM_OFFSET = 1000; //pixels
 
 const ProductList = () => {
-  const [page, setPage] = useState(1);
   const [totalList, setTotalList] = useState<Product[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(DEFAULT_PAGE);
 
-  const increasePage = () => {
-    setPage((prevPage) => prevPage + 1);
+  //Using debounce to limit the number of API calls
+  //when the user spams scroll event
+  const increasePage = debounce(() => {
+    if (!isLoading) setPage((prevPage) => prevPage + 1);
+  }, DEBOUNCE_DELAY);
+
+  const handleSearchChange = (value: string) => {
+    setSearchQuery(value);
   };
 
-  const debouncedIncreasePage = debounce(increasePage, DEBOUNCE_DELAY);
+  const resetParams = () => {
+    setPage(1);
+    setTotalList([]);
+  };
 
-  // Fetch data from the API
-  const { data, isLoading, error } = useFetch<ResponseData>(`https://dummyjson.com/products?limit=${PRODUCTS_PER_PAGE}&skip=${getSkipValue(page)}`);
+  // Fetch products from the API
+  const { products, isLoading, error } = useProductFetch({
+    searchQuery,
+    page,
+    pageLimit: PRODUCTS_PER_PAGE,
+  });
 
-  useInfiniteScroll(debouncedIncreasePage, 0);
+  useInfiniteScroll(increasePage, BOTTOM_OFFSET);
 
+  //Reset the page number and totalList when the search query changes
   useEffect(() => {
-    if (data?.products) {
-      setTotalList((prevList) => [...prevList, ...data.products]);
+    resetParams();
+  }, [searchQuery]);
+
+  // Append new data to the totalList when data changes
+  useEffect(() => {
+    if (products) {
+      setTotalList((prevList) => [...prevList, ...products]);
     }
-  }, [data]);
+  }, [products]);
 
   const hasNoData = !isLoading && totalList.length === 0;
 
   return (
     <>
+      <Input onChange={handleSearchChange} placeholder="Search for products..." label="Search Products" />
       {error && <p>{error}</p>}
       <div className="product-list">
         {totalList.length > 0 && totalList.map((product) => <ProductCard key={product.id} product={product} />)}
