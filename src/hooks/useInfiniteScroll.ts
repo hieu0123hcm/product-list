07854bc09fb8369957e519fceb74d461ebf17208
Nguyễn Thanh Hiếu <43198687+hieu0123hcm@users.noreply.github.com>
2025-04-22 15:44:46 +0700

    fix: change scroll logic

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 const useInfiniteScroll = (callback: () => void, offset: number = 0) => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.innerHeight + window.scrollY;
-      const bottomPosition = document.documentElement.scrollHeight + offset;
+      const bottomPosition = document.documentElement.scrollHeight - offset;
 
       if (scrollPosition >= bottomPosition) {
         callback();
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, [callback, offset]);
 };
 
